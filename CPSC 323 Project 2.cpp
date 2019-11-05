78ea9a142abba658b41698243777f58714081b6e
jcastillo2583 <bluejccastle@gmail.com>
2019-11-05 08:48:57 -0800

    Add files via upload

---
+++ CPSC 323 Project 2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<fstream>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+#include<type_traits>
+
+using namespace std;
+
+int main() {
+	
+	//cout << "Program complete" << endl << endl;
+
+	return 0;
+}
